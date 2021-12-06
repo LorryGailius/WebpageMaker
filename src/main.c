c1d86f3070ef7eaf91a8f67567252ef46feba6eb
Laurynas Gailius <lauris.gailius@gmail.com>
2021-12-06 11:42:17 +0200

    Update main.c
    Co-Authored-By: Arnas Vaicekauskas <32205690+LionasBaslis2sis@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void Func(int x);
+
 int main(int argc, char **argv) {
 	printf("Puslapis!");
 	return EXIT_SUCCESS;
+}
+
+
+void Func(int x)
+{
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf("%d\n",i);
+    }
 }
