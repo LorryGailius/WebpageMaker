7bc42c9872cc9233e98bf782c438fc339fdbad73
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2021-12-06 12:49:36 +0200

    Panaikinta build direktorija.

--- build/CMakeFiles/WebPageMaker.dir/src/main.c.obj.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/WebPageMaker.dir/src/main.c.obj: \
- C:\Users\Lorry\Documents\GitHub\WebpageMaker\src\main.c \
- c:\mingw\include\stdlib.h c:\mingw\include\_mingw.h \
- c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
- c:\mingw\include\sdkddkver.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h c:\mingw\include\stdio.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h
