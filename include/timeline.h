e79a6a29ef8dc146fd99cc1038a37e876c5072c6
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2021-12-12 22:43:17 +0200

    Made timeline.c more readable
    Separated create_timeline_html() into several functions in order to increase readability.

--- include/timeline.h
+++ include/timeline.h
@@ -4,6 +4,12 @@
 #include <html_doc.h>
 #include <user_input.h>
 
-HTML_doc_t *create_timeline_html(char *title, char *description, data_t *data, size_t post_cnt);
+HTML_doc_t *create_timeline_html(char *title, char *description, data_t *data, size_t size);
+
+elem_t* create_timeline_post_section(data_t *data, size_t size);
+
+elem_t* create_timeline_style();
+
+elem_t* create_timeline_footer();
 
 #endif /* TIMELINE_H */
--- src/timeline.c
+++ src/timeline.c
@@ -1,94 +1,66 @@
 #include <timeline.h>
 
-HTML_doc_t *create_timeline_html(char *title, char *description, data_t *data, size_t post_cnt) {
+HTML_doc_t *create_timeline_html(char *title, char *description, data_t *data, size_t size) {
+    // head elements
     elem_t* head_elems[3];
     head_elems[0] = create_tag_element(TAG_META);
+    push_element_attribute(head_elems[0], create_attribute(ATTR_NAME, "viewport"));
+    push_element_attribute(head_elems[0], create_attribute(ATTR_CONTENT, "width=device-width, initial-scale=1.0"));
+
     head_elems[1] = create_tag_element(TAG_TITLE);
-    head_elems[2] = create_tag_element(TAG_STYLE);
+    push_element(head_elems[1], create_text_element(title ? title : ""));
 
-    attr_t *meta_attr[2];
-    meta_attr[0] = create_attribute(ATTR_NAME, "viewport");
-    meta_attr[1] = create_attribute(ATTR_CONTENT, "width=device-width, initial-scale=1.0");
-
-    push_element_attribute(head_elems[0], meta_attr[0]);
-    push_element_attribute(head_elems[0], meta_attr[1]);
-
-    elem_t *title_elem = create_text_element(title ? title : "");
-    push_element(head_elems[1], title_elem);
-
-    FILE *css_file = fopen("bin/style.css", "rb");
-    char *src = NULL;
-    if(css_file == NULL) {
-        log_msg("create_timeline_html(): /bin/style.css is missing.");
-    } else {
-        log_msg("create_timeline_html(): opened /bin/style.css");
-        fseek(css_file, 0, SEEK_END);
-        size_t src_size = ftell(css_file);
-        rewind(css_file);
-        src = (char*) malloc(src_size);
-        size_t read = fread(src, sizeof(char), src_size, css_file);
-        if(read != src_size) {
-            log_msg("create_timeline_html(): /bin/style.css could not be read.");
-        } else {
-            log_msg("create_timeline_html(): /bin/style.css has been read.");
-        }
-        fclose(css_file);
-        log_msg("create_timeline_html(): /bin/style.css has been closed.");
-    }
-    
-    elem_t *style_src = create_text_element(src);
-    push_element(head_elems[2], style_src);
-
+    head_elems[2] = create_timeline_style();
+    // body elements
     elem_t *body_elems[] = {
         create_tag_element(TAG_H1),
         create_tag_element(TAG_H3),
-        create_tag_element(TAG_DIV),
-        create_tag_element(TAG_FOOTER)
+        create_timeline_post_section(data, size),
+        create_timeline_footer()
     };
 
     if(title != NULL) {
-        elem_t *h_text = create_text_element(title);
-        attr_t *h_attr = create_attribute(ATTR_STYLE, "text-align: center; padding-top: 1%;color:white;font-size:350%;");
-        push_element_attribute(body_elems[0], h_attr);
-        push_element(body_elems[0], h_text);
+        const char * attr_str = "text-align: center; padding-top: 1%;color:white;font-size:350%;";
+        push_element_attribute(body_elems[0], create_attribute(ATTR_STYLE, attr_str));
+        push_element(body_elems[0], create_text_element(title));
     }
 
     if(description != NULL) {
-        elem_t *d_text = create_text_element(description ? description : "");
-        attr_t *d_attr = create_attribute(ATTR_STYLE, "text-align: center; padding-bottom: 5%;color:white;");
-        push_element_attribute(body_elems[1], d_attr);
-        push_element(body_elems[1], d_text);
+        const char* attr_str = "text-align: center; padding-bottom: 5%;color:white;";
+        push_element_attribute(body_elems[1], create_attribute(ATTR_STYLE, attr_str));
+        push_element(body_elems[1], create_text_element(description));
     }
 
-    attr_t *tl_attr = create_attribute(ATTR_CLASS, "timeline");
-    push_element_attribute(body_elems[2], tl_attr);
+    push_element_attribute(body_elems[2], create_attribute(ATTR_CLASS, "timeline"));
+    return create_doc(head_elems, 3, body_elems, 4);
+}
 
-    for(size_t i = 0; i < post_cnt; ++i) {
-        if(data[i].header == NULL && data[i].description == NULL &&data[i].image == NULL) continue;
+elem_t* create_timeline_post_section(data_t *data, size_t size) {
+    elem_t *section = create_tag_element(TAG_DIV);
+    for(size_t i = 0; i < size; ++i) {
+        if((data[i].header == NULL) && (data[i].description == NULL) && (data[i].image == NULL)) continue;
+
         elem_t *inside_div = create_tag_element(TAG_DIV);
         attr_t *idiv_class = create_attribute(ATTR_CLASS, "content");   
         push_element_attribute(inside_div, idiv_class);
 
         if(data[i].header != NULL) {
-            elem_t *year = create_text_element(data[i].header);
             elem_t *h2 = create_tag_element(TAG_H2);
-            push_element(h2, year);
+            push_element(h2, create_text_element(data[i].header));
             push_element(inside_div, h2);
         }
 
         if(data[i].description != NULL) {
-            elem_t *text = create_text_element(data[i].description);
-            elem_t *par =  create_tag_element(TAG_PAR);
-            push_element(par, text);
+            elem_t *par = create_tag_element(TAG_PAR);
+            push_element(par, create_text_element(data[i].description));
             push_element(inside_div, par);
         }
 
         if(data[i].image != NULL) {
             elem_t *img = create_tag_element(TAG_IMG);
-            attr_t *img_src = create_attribute(ATTR_SRC, data[i].image);
-            attr_t *img_style = create_attribute(ATTR_STYLE, "height: 100%; width: 100%; object-fit: contain;display:block;margin:auto");
-            push_element_attribute(img, img_src);
-            push_element_attribute(img, img_style);
+            const char* attr_str = "height: 100%; width: 100%; object-fit: contain;display:block;margin:auto";
+            push_element_attribute(img, create_attribute(ATTR_SRC, data[i].image));
+            push_element_attribute(img, create_attribute(ATTR_STYLE, attr_str));
             push_element(inside_div, img);
         }
 
@@ -97,32 +69,47 @@ HTML_doc_t *create_timeline_html(char *title, char *description, data_t *data, s
         attr_t *odiv_class = create_attribute(ATTR_CLASS, val);
         push_element_attribute(outside_div, odiv_class);
         push_element(outside_div, inside_div);
-        push_element(body_elems[2], outside_div);
+        push_element(section, outside_div);
     }
+    return section;
+}
 
-    // footer magic
-
-    elem_t *f_div = create_tag_element(TAG_DIV);
-    attr_t *f_div_class = create_attribute(ATTR_CLASS, "footer-content");
-    push_element_attribute(f_div, f_div_class);
-    push_element(body_elems[3], f_div);
-    // footer header 3
-    elem_t *f_h3 = create_tag_element(TAG_H3);
-    elem_t *f_h3_text = create_text_element("WebpageMaker");
-    push_element(f_h3, f_h3_text);
-    push_element(f_div, f_h3);
-
-    elem_t *f_p = create_tag_element(TAG_PAR);
-    // inside text tags are not supported
-    elem_t *f_p_text = create_text_element("Made with<a href=\"https://github.com/LorryGailius/WebpageMaker\">WebpageMaker</a>");
-    push_element(f_p, f_p_text);
-    push_element(f_div, f_p);
-
-    HTML_doc_t *doc = create_doc(head_elems, 3, body_elems, 4);
-    if(doc == NULL) {
-        log_msg("create_timeline_html(): doc could not be created, returned NULL.");
+elem_t* create_timeline_style() {
+    FILE *handle = fopen("bin/style.css", "rb");
+    if(handle == NULL) {
+        fprintf(stderr, "create_timeline_style(): /bin/style.css is missing, returned NULL.\n");
         return NULL;
     }
-    log_msg("create_timeline_html(): doc successfully created.");
-    return doc;
+    size_t file_size = get_file_size("bin/style.css");
+    if(file_size == 0) {
+        fprintf(stderr, "create_timeline_style(): /bin/style.css is empty, style is NULL.\n");
+        return NULL;
+    }
+
+    char *src = (char*) malloc(file_size);
+    fread(src, sizeof(char), file_size, handle);
+    fclose(handle);
+
+    elem_t *style = create_tag_element(TAG_STYLE);
+    push_element(style, create_text_element(src));
+    return style;
+}
+
+elem_t* create_timeline_footer() {
+    elem_t *footer = create_tag_element(TAG_FOOTER);
+    elem_t *div = create_tag_element(TAG_DIV);
+    attr_t *attrib = create_attribute(ATTR_CLASS, "footer-content");
+    push_element_attribute(div, attrib);
+    // footer header
+    elem_t *heading = create_tag_element(TAG_H3);
+    elem_t *heading_text = create_text_element("WebpageMaker");
+    push_element(heading, heading_text);
+    push_element(div, heading);
+    // footer paragraph
+    elem_t *par = create_tag_element(TAG_PAR);
+    elem_t *par_text = create_text_element("Made with<a href=\"https://github.com/LorryGailius/WebpageMaker\">WebpageMaker</a>");
+    push_element(par, par_text);
+    push_element(div, par);
+    push_element(footer, div);
+    return footer;
 }
